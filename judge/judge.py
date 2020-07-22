ac196364cd45f5bfe95e9048db97408a853cb8b1
Zhicheng Zhang <zhangzhicheng1@126.com>
2020-07-22 16:44:16 +0800

    support for windows

--- judge/judge.py
+++ judge/judge.py
@@ -1,8 +1,35 @@
 import subprocess
-import timeout_decorator
 import time
 import numpy as np
 import sys
+
+import signal
+
+class TimeoutError(Exception):
+    def __init__(self, value = "Timed Out"):
+        self.value = value
+    def __str__(self):
+        return repr(self.value)
+
+def timeout(seconds_before_timeout):
+    def decorate(f):
+        def handler(signum, frame):
+            raise TimeoutError()
+        def new_f(*args, **kwargs):
+            old = signal.signal(signal.SIGALRM, handler)
+            signal.alarm(seconds_before_timeout)
+            try:
+                result = f(*args, **kwargs)
+            finally:
+                # reinstall the old signal handler
+                signal.signal(signal.SIGALRM, old)
+                # cancel the alarm
+                # this line should be inside the "finally" block (per Sam Kortchmar)
+                signal.alarm(0)
+            return result
+        new_f.__name__ = f.__name__
+        return new_f
+    return decorate
 
 def win(id):
     if id == 2:
@@ -30,7 +57,7 @@ class AI:
     def receive(self):
         return self.proc.stdout.readline().strip().decode()
 
-    @timeout_decorator.timeout(5)
+    @timeout(5)
     def init(self):
         if self.human == 0:
             self.proc = subprocess.Popen(self.path,
@@ -39,7 +66,7 @@ class AI:
             self.send(self.id)
             self.name = self.receive()
 
-    @timeout_decorator.timeout(5)
+    @timeout(5)
     def action(self, a, b):
         if self.human == 1:
             value = sys.stdin.readline().strip().split(' ')
